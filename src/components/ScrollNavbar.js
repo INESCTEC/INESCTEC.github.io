9297e988260de0e0911f6566fe83a75115ecc343
Mário Branco <mario.branco18@gmail.com>
2024-11-11 15:20:29 +0000

    Changed Navbar to white version

--- src/components/ScrollNavbar.js
+++ src/components/ScrollNavbar.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import logo from '../assets/INESCTEC_logotipo_monocrom_white.png';
+import logo from '../assets/INESCTEC_logotipo_color_rgb.png';
 
 const ScrollNavbar = () => {
   const [showNavbar, setShowNavbar] = useState(false);
@@ -19,33 +19,33 @@ const ScrollNavbar = () => {
     <div
       className={`fixed top-0 left-0 w-full z-20 shadow-md transition-opacity duration-300 ${
         showNavbar ? 'opacity-100' : 'opacity-0'
-      } navbar-gradient navbar-large`}
+      } bg-white navbar-large`}
     >
       <nav className="flex justify-between items-center p-8">
         <div className="flex items-end">
           <Link to="/">
             <img src={logo} alt="Logo" className="h-8 w-auto md:h-10 w-auto" />
           </Link>
-          <span className="hidden md:block ml-3 text-white text-xl self-end">Open Source Software</span>
+          <span className="hidden md:block ml-3 text-dark-blue-2 font-bold text-xl self-end">OSS</span>
         </div>
         
-        <ul className="flex items-center space-x-4 text-white text-lg">
+        <ul className="flex items-center space-x-4 text-dark-blue-2 font-semibold text-lg">
           <li>
             <Link
               to="/"
               className={`pb-2 ${
-                location.pathname === '/' ? 'border-b-2 border-white' : ''
+                location.pathname === '/' ? 'border-b-2 border-light-blue-2' : ''
               }`}
             >
               Home
             </Link>
           </li>
-          <span className="text-white">|</span>
+          <span className="text-dark-blue-2">|</span>
           <li>
             <Link
               to="/projects"
               className={` ${
-                location.pathname === '/projects' ? 'border-b-2 border-white' : ''
+                location.pathname === '/projects' ? 'border-b-2 border-dark-blue-2' : ''
               }`}
             >
               Projects
