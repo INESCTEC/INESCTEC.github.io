70683d163bf156c80f1e464bf3e62b0b12f43904
itsjrsa <jrsa2012@gmail.com>
2024-11-24 23:14:20 +0000

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,9 @@ const Footer = () => {
     <footer className="bg-gradient-to-r from-dark-blue to-light-blue text-white py-4 font-mono">
       <div className="flex flex-col md:flex-row justify-center md:justify-between items-center max-w-7xl mx-auto px-4">
         <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-14 text-sm md:text-base">
-          <a href="https://inesctec.pt" className="hover:underline">Contacts</a>
-          <a href="https://inesctec.pt" className="hover:underline">Code of Conduct</a>
-          <a href="https://inesctec.pt" className="hover:underline">Copyright</a>
+          <a href="https://www.inesctec.pt/en/get-in-touch" className="hover:underline">Contacts</a>
+          <a href="https://github.com/INESCTEC/.github/blob/main/documents/code_of_conduct.md" className="hover:underline">Code of Conduct</a>
+          <a href="https://inesctec.pt" className="hover:underline">Copyright @ INESC TEC 2024</a>
         </div>
         <div className="mt-4 md:mt-0 text-center md:text-right text-sm md:text-base">
           2024, INESC TEC
