ed9669cd6d8e1d79bf4ff243b6def1edbe9d8407
Mário Branco <mario.branco18@gmail.com>
2024-12-15 22:41:40 +0000

    routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage';
 import ProjectsPage from './pages/ProjectsPage';
 
 function App() {
   return (
-    <Router basename="/">
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/projects" element={<ProjectsPage />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/projects" element={<ProjectsPage />} />
+      </Routes>
+    </div>
   );
 }
 
